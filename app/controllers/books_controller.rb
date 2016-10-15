5474457a06d8e3304e3816ba92b26deae456e21e
bradmin82 <bweldy82@gmail.com>
2016-10-15 15:58:24 -0700

    Switching to postgres as main database provider, locally and in production.

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,10 +5,12 @@ class BooksController < ApplicationController
 	def index
 		if params[:category].blank?
 			@books = Book.all.order("created_at DESC")
+			p "Cats blank"
 		else
 			@category_id = Category.find_by(name: params[:category]).id
 			@books = Book.where(:category_id => @category_id).order("created_at DESC")
 		end
+
 	end
 
 	def show
